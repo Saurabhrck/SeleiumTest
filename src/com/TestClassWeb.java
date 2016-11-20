2e73301fa46b1ee6b607be5f0df7e3c2ec30dcae
Saurabhrck <nxgen22@gmail.com>
2016-11-21 00:34:17 +0530

    change v1.2

--- src/com/TestClassWeb.java
+++ src/com/TestClassWeb.java
@@ -1,29 +1,21 @@
 package com;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class TestClassWeb {
 
 	public static String user="data", pass="data";
-	
-	
 	public static void main(String []args) throws InterruptedException{
 		login();
 	}
-	
 	public static WebDriver initial(){
 		System.setProperty("webdriver.chrome.driver", "C://Users//saumo//Downloads//Compressed//chromedriver_win32//chromedriver.exe");
 		WebDriver driverr=new ChromeDriver();
 		return driverr;
 	}
-	
 	public static void login(){
 		WebDriver driver=initial();
 		driver.get("https://www.amazon.in/");
@@ -37,9 +29,6 @@ public class TestClassWeb {
 		driver.findElement(By.id("ap_password")).sendKeys(Keys.RETURN);
 		driver.findElement(By.xpath(".//*[@id='nav-link-yourAccount']/span[1]")).click();
 		driver.findElement(By.xpath(".//*[@id='a-page']/div[1]/div/div[4]/div/div/div[2]/div/div[2]/ul[1]/li[2]/span/a")).click();
-		System.out.println(driver.getTitle());
-		
-		
-		
+		System.out.println(driver.getTitle());	
 	}
 }
